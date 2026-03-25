cb7051411989f4bdb3069e5828db52041b2f4397
Alexander Drozdoff <anomalocaribd@gmail.com>
2026-03-25 16:45:47 +1300

    Limit icon thumbnail pass to first kickoff item

--- src/view-file/view-file-icon.h
+++ src/view-file/view-file-icon.h
@@ -101,7 +101,7 @@ void vficon_selection_to_mark(ViewFile *vf, gint mark, SelectionToMarkMode mode)
 void vficon_thumb_progress_count(const GList *list, gint &count, gint &done);
 void vficon_read_metadata_progress_count(const GList *list, gint &count, gint &done);
 void vficon_set_thumb_fd(ViewFile *vf, FileData *fd);
-FileData *vficon_thumb_next_fd(ViewFile *vf);
+FileData *vficon_thumb_next_fd(ViewFile *vf, gboolean force_first);
 
 FileData *vficon_star_next_fd(ViewFile *vf);
 void vficon_set_star_fd(ViewFile *vf, FileData *fd);
